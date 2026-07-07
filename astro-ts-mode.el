0d4a3b14f7871aecb970e7de9e3b44277cf8d3c5
Ruby Iris Juric <ruby@srxl.me>
2026-07-07 13:32:05 +1000

    add entry to treesit-language-source-alist on require

--- astro-ts-mode.el
+++ astro-ts-mode.el
@@ -39,6 +39,15 @@
 (require 'css-mode)
 (require 'html-ts-mode)
 
+(eval-when-compile
+  (require 'rx))
+
+(add-to-list
+ 'treesit-language-source-alist
+ '(astro "https://github.com/virchau13/tree-sitter-astro"
+         :commit "213f6e6973d9b456c6e50e86f19f66877e7ef0ee")
+ t)
+
 (defgroup astro ()
   "Major mode for editing Astro templates."
   :group 'languages)
